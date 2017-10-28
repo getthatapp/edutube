3b1296344d45a8df95eacd89faa6c072d13a9686
Grzegorz <apps@getthatapp.pl>
2017-10-28 12:04:06 +0200

    All methods in links_controller

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,31 +1,50 @@
 class LinksController < ApplicationController
 
   def index
-
+    @links = Link.all.order(created_at: :DESC)
   end
 
   def new
-
+    @link - Link.new
   end
 
   def create
-
+    @link = Link.new(link_params)
+    if @link.save
+      redirect_to root_path, success: "Link created!"
+    else
+      render 'new'
+    end
   end
 
   def show
-
+    link
   end
 
   def edit
-
+    link
   end
 
   def update
-
+    if link_update(link_params)
+      redirect_to link_path(link), info: "Link updated!"
+    else
+      render 'edit'
+    end
   end
 
   def destroy
 
+  end
+
+  private
+
+  def link_params
+    params.require(:link).permit(:title, :link)
+  end
+
+  def link
+    @link = Link.find(params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root 'home#home'
+  get 'home', to: 'home#home'
   resources :links
 end
